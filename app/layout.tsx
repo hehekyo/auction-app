460318d2309e09455d28e39b19b2de449977f376
alex <>
2024-12-31 11:15:53 +0900

    update: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,47 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ConnectkitProvider } from './connectkit-provider'
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import { Providers } from './providers';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ConnectkitProvider } from "./connectkit-provider";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Providers } from "./providers";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DAuction',
-  description: 'DAuction',
-}
+  title: "DAuction",
+  description: "DAuction",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <Providers>
           <div className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen flex flex-col">
-            
             <main className="flex-grow">
               <ConnectkitProvider
                 customTheme={{
                   "--ck-accent-color": "#00D54B",
                   "--ck-accent-text-color": "#ffffff",
                 }}
-              > <Header />
+              >
+                {" "}
+                <Header />
                 {children}
-                </ConnectkitProvider>
+              </ConnectkitProvider>
             </main>
             <Footer />
           </div>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
